9811ae068d2f4abfe38a4f8f43b1bf60b8a1a206
your Carrot <stylenbs@gmail.com>
2020-12-17 18:50:23 +0900

    router 기본설정

---
+++ server/routes/guestbook.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express.Router();
+const { User } = require("../models/User");
+
+const { auth } = require("../middleware/auth");
+
+//=================================
+//          Guest Book
+//=================================
+
+module.exports = router;
